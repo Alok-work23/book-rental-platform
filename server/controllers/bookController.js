814b0d6270f1edd6c6a47c28cba4bf53e9493377
Alok Raj <alokwork2016@gmail.com>
2025-08-03 12:30:32 +0530

    Add book routes and controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,6 +1,6 @@
-const Book = require("../models/Book");
+const Book = require('../models/Book');
 
-// Create Book
+// POST: Add new book
 exports.createBook = async (req, res) => {
   try {
     const book = new Book(req.body);
@@ -11,11 +11,31 @@ exports.createBook = async (req, res) => {
   }
 };
 
-// Get All Books
+// GET: All books
 exports.getBooks = async (req, res) => {
   try {
     const books = await Book.find();
     res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// PUT: Update book
+exports.updateBook = async (req, res) => {
+  try {
+    const updated = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// DELETE: Remove book
+exports.deleteBook = async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ msg: "Book deleted" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
